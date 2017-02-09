380d64074efb2f2462264ddaf1725ab52e104ac1
Nancy Blayney <nblayney@g.cofc.edu>
2017-02-09 17:30:29 -0500

    initial commit

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ $(document).ready(function(){
 	function parallaxbubbles(){
 	  var scrolltop = $(window).scrollTop();
 	  var scrollamount = (scrolltop / (scrollheight-windowheight)) * 100;	
+	  $('.shark').css('left', -100 + scrollamount + '%');
 	  $('.seahorse').css('top', -scrolltop * .2 + 'px');
 	  $('.turtle').css('top', -scrolltop * .5 + 'px');
 	 	$('.bubble').css('top', -scrolltop * .8 + 'px');
 	  $('.bubble2').css('top', -scrolltop * .9 + 'px');
-	  $('.shark').css('left', -100 + scrollamount + '%');
 	}
 	 
 	$(window).scroll(function(e){ 
